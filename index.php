1e508f31a22d65dcffcc8c01bda254db013a4047
João Paulo Goulart Matos <joaopgoulart10@gmail.com>
2024-04-23 09:56:27 -0300

    commit

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php 
+    include_once("templates/templates.php")
+?>
+
+    <h1>teste</h1>
+</body>
+</html>
---
+++ templates/templates.php
@@ -0,0 +1,19 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="<?= $BASE_URL?>css/style.css">
+</head>
+<body>
+    <header> 
+        <p>teste</p>
+    </header>
